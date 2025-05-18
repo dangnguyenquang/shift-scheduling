f72e75f9f008d88bf12a0131668a33bf3dd89d79
NguyenQuangDang <dangnguyenquangit@gmail.com>
2025-05-18 22:12:31 +0700

    first commit

---
+++ src/test/java/com/example/shift_scheduling/ShiftSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shift_scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
